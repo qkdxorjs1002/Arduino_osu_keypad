8e4987cbcbc8f1a5325f82748c3c70f0dd3fc179
Novang <qkdxorjs1002@naver.com>
2016-02-07 21:28:15 +0900

    v2

--- AXFNLED.ino
+++ AXFNLED.ino
@@ -9,9 +9,13 @@
 #define LEDPIN_A    9         // A LED pin number
 #define LEDPIN_X    10        // X LED pin number
 #define LEDPIN_FN   11        // FN LED pin number
+#define LED_FD      1         // LED Fading
 #define PROCESS_SPD 1         // Process speed (ms)
 
 int LED_DV = 127;   // Default LED Bri. value
+int LED_TMA = LED_DV;     // Temp Value
+int LED_TMX = LED_DV;     // Temp Value
+int LED_TMFN = LED_DV;     // Temp Value
 int VALUE_A = 0;    // For Multi-key Input processing
 int VALUE_X = 0;    // For Multi-key Input processing
 int VALUE_FN = 0;   // For Multi-key Input processing
@@ -32,11 +36,15 @@ void setup() {
   analogWrite(LEDPIN_X,LED_DV);
   analogWrite(LEDPIN_FN,LED_DV);
   LED_DV = EEPROM.read(1);    // Read saved LED Bri. value from EEPROM
-  delay(1000);
+  LED_TMA = LED_DV;
+  LED_TMX = LED_DV;
+  LED_TMFN = LED_DV;
+  delay(500);
+  Keyboard.begin();
+  delay(500);
   analogWrite(LEDPIN_A,0);
   analogWrite(LEDPIN_X,0);
   analogWrite(LEDPIN_FN,0);
-  Keyboard.begin();
 }
 
 /* 
@@ -62,6 +70,15 @@ void debug() {
 }
 
 /*
+ * Read pin connection value
+ */
+void READPIN() {
+  VALUE_A = digitalRead(KEYPIN_A);
+  VALUE_X = digitalRead(KEYPIN_X);
+  VALUE_FN = digitalRead(KEYPIN_FN);
+}
+
+/*
  * Loop
  */
 void loop() {
@@ -71,33 +88,17 @@ void loop() {
   delay(PROCESS_SPD);
   
   //For Multi-key Input processing
-  VALUE_A = digitalRead(KEYPIN_A);
-  VALUE_X = digitalRead(KEYPIN_X);
-  VALUE_FN = digitalRead(KEYPIN_FN);
+  READPIN();
   
-  // FN key: FN+A(LED Bri. set), FN+X(Save Bri. value to EEPROM)
+  // FN key: FN+A(LED Bri. set), FN+X(Toggle LED Fading), FN+AX(Save Bri. value to EEPROM)
   if(VALUE_FN == 0){
     analogWrite(LEDPIN_FN,LED_DV);
+    LED_TMFN = LED_DV;
     
     // FN+A(LED Bri. set)
     if(VALUE_A == 0){
-      analogWrite(LEDPIN_A,LED_DV);
-      if(VALUE_X == 0){
-        delay(3000);
-        analogWrite(LEDPIN_X,LED_DV);
-        if(LED_DV == EEPROM.read(1)){
-          // Blank
-        }else{
-          analogWrite(LEDPIN_A,0);
-          analogWrite(LEDPIN_X,0);
-          analogWrite(LEDPIN_FN,LED_DV);
-          EEPROM.write(1, LED_DV);
-          delay(2000);
-          analogWrite(LEDPIN_A,LED_DV);
-          analogWrite(LEDPIN_X,LED_DV);
-          analogWrite(LEDPIN_FN,LED_DV);
-        }
-      }
+      VALUE_A = 1;
+      VALUE_FN = 1;
       if(LED_DV == 0){
         LED_DV = 51;
       }else if(LED_DV == 51){
@@ -111,47 +112,77 @@ void loop() {
       }else if(LED_DV == 255){
         LED_DV = 0;
       }
+      LED_TMA = LED_DV;
       analogWrite(LEDPIN_A,LED_DV);
       analogWrite(LEDPIN_FN,LED_DV);
       delay(500);
     }
     
-    // FN+X(Save Bri. value to EEPROM)
+    // FN+X(Toggle LED Fading)
     if(VALUE_X == 0){
-        delay(3000);
+      VALUE_X = 1;
+      VALUE_FN = 1;
+      LED_TMA = LED_DV;
+      LED_TMX = LED_DV;
+      if(LED_DV == EEPROM.read(1)){
+        // Blank
+      }else{
+        analogWrite(LEDPIN_A,0);
+        analogWrite(LEDPIN_X,0);
+        analogWrite(LEDPIN_FN,LED_DV);
+        EEPROM.write(1, LED_DV);
+        delay(1000);
+        analogWrite(LEDPIN_A,LED_DV);
         analogWrite(LEDPIN_X,LED_DV);
-        if(LED_DV == EEPROM.read(1)){
-          // Blank
-        }else{
-          analogWrite(LEDPIN_A,0);
-          analogWrite(LEDPIN_X,0);
-          analogWrite(LEDPIN_FN,LED_DV);
-          EEPROM.write(1, LED_DV);
-          delay(2000);
-          analogWrite(LEDPIN_A,LED_DV);
-          analogWrite(LEDPIN_X,LED_DV);
-          analogWrite(LEDPIN_FN,LED_DV);
-        }
+        analogWrite(LEDPIN_FN,LED_DV);
       }
+      analogWrite(LEDPIN_A,LED_DV);
+      analogWrite(LEDPIN_X,LED_DV);
+      analogWrite(LEDPIN_FN,LED_DV);
+    }
   }else if(VALUE_FN == 1){
     analogWrite(LEDPIN_FN,0);
+    if(LED_FD == 1){
+      if(LED_TMFN > 0){
+        LED_TMFN = LED_TMFN - 1;
+        analogWrite(LEDPIN_FN, LED_TMFN);
+      }
+    }else{
+      analogWrite(LEDPIN_FN, 0);
+    }
   }
   
   // A Key
   if(VALUE_A == 0){
     Keyboard.press('a');
     analogWrite(LEDPIN_A,LED_DV);
+    LED_TMA = LED_DV;
   }else if(VALUE_A == 1){
     Keyboard.release('a');
-    analogWrite(LEDPIN_A,0);
+    if(LED_FD == 1){
+      if(LED_TMA > 0){
+        LED_TMA = LED_TMA - 1;
+        analogWrite(LEDPIN_A, LED_TMA);
+      }
+    }else{
+      analogWrite(LEDPIN_A, 0);
+    }
   }
   
   // X Key
   if(VALUE_X == 0){
     Keyboard.press('x');
     analogWrite(LEDPIN_X,LED_DV);
+    LED_TMX = LED_DV;
   }else if(VALUE_X == 1) {
     Keyboard.release('x');
-    analogWrite(LEDPIN_X,0);
+    if(LED_FD == 1){
+      if(LED_TMX > 0){
+        LED_TMX = LED_TMX - 1;
+        analogWrite(LEDPIN_X, LED_TMX);
+      }
+    }else{
+      analogWrite(LEDPIN_X, 0);
+    }
   }
 }
